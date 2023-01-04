d5e64092df40c5ede30d5f9438dfc04a957a0735
Radoslav Bekyarov <rbekyarov82@gmail.com>
2023-01-04 16:28:09 +0200

    up

--- src/main/java/softuni/exam/InitData.java
+++ src/main/java/softuni/exam/InitData.java
@@ -34,10 +34,12 @@ public class InitData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        //ADD USER
         if (userRepository.findAll().size() == 0) {
             userRepository.save(new User("admin", "admin", Role.ADMIN));
             userRepository.save(new User("user", "user", Role.USER));
         }
+        //ADD Dog Behavior
         if (behaviorRepository.findAll().size() == 0) {
             behaviorRepository.save(new Behavior("Good with dogs"));
             behaviorRepository.save(new Behavior("Good with dogs and humans"));
@@ -48,14 +50,17 @@ public class InitData implements CommandLineRunner {
             behaviorRepository.save(new Behavior("Cowardly dog"));
 
         }
+        //ADD Dog Breed
         if (breedRepository.findAll().size() == 0) {
             breedRepository.save(new Breed("Boxer"));
             breedRepository.save(new Breed("German Shepherd"));
             breedRepository.save(new Breed("Rotwailer"));
             breedRepository.save(new Breed("Bulgarian Shepherd - BOK"));
             breedRepository.save(new Breed("Miniature Schnauzer"));
+            breedRepository.save(new Breed("Huske"));
 
         }
+        //ADD Dog House
         if (cellRepository.findAll().size() == 0) {
             cellRepository.save(new Cell("A1", Status.empty));
             cellRepository.save(new Cell("A2", Status.busy));
@@ -63,8 +68,11 @@ public class InitData implements CommandLineRunner {
             cellRepository.save(new Cell("B1", Status.empty));
             cellRepository.save(new Cell("B2", Status.busy));
             cellRepository.save(new Cell("B3", Status.empty));
+            cellRepository.save(new Cell("C1", Status.empty));
+            cellRepository.save(new Cell("C2", Status.empty));
 
         }
+        //ADD Cities
         if (cityRepository.findAll().size() == 0) {
             cityRepository.save(new City("1000", "Sofia"));
             cityRepository.save(new City("2000", "Plovdiv"));
@@ -73,8 +81,19 @@ public class InitData implements CommandLineRunner {
             cityRepository.save(new City("7008", "Ruse"));
 
         }
+        //ADD PRICES
         if (priceRepository.findAll().size() == 0) {
-            priceRepository.save(new Price(BigDecimal.valueOf(20.00),
+            priceRepository.save(new Price(
+                    BigDecimal.valueOf(15.00),
+                    BigDecimal.valueOf(9.00),
+                    BigDecimal.valueOf(5.00),
+                    BigDecimal.valueOf(5.00),
+                    BigDecimal.valueOf(5.00),
+                    BigDecimal.valueOf(5.00),
+                    BigDecimal.valueOf(5.00),
+                    BigDecimal.valueOf(5.00)));
+            priceRepository.save(new Price(
+                    BigDecimal.valueOf(20.00),
                     BigDecimal.valueOf(10.00),
                     BigDecimal.valueOf(8.00),
                     BigDecimal.valueOf(7.00),
@@ -85,15 +104,18 @@ public class InitData implements CommandLineRunner {
 
 
         }
+        //ADD CLIENT
         if (clientRepository.findAll().size() == 0) {
             clientRepository.save(new Client("Ivan", "Ivanov", "ivanov@gmail.com", "0886335241", "Lulin 15", cityRepository.getById(Long.parseLong("1"))));
-            clientRepository.save(new Client("Stanimir", "Pavlov", "spavlov@abv.bg", "08842276361", "Zdravetz 4", cityRepository.getById(Long.parseLong("2"))));
+            clientRepository.save(new Client("Stanimir", "Pavlov", "spavlov@abv.bg", "08842276361", "Zdravetzh 4", cityRepository.getById(Long.parseLong("2"))));
             clientRepository.save(new Client("Vladimir", "Georgiev", "vgeorgiev@outlook.com", "0887325579", "Zagorka 16", cityRepository.getById(Long.parseLong("3"))));
+            clientRepository.save(new Client("Boyan", "Dimitrov", "bdimitrov@hotmail.com", "0881256377", "Ludogorska 12B", cityRepository.getById(Long.parseLong("4"))));
 
 
         }
-
+        //ADD DOG
         if (dogRepository.findAll().size() == 0) {
+            //New Dog "Jerry"
             dogRepository.save(new Dog("Jerry",
                     LocalDate.of(2018, 7, 15),
                     "jerry.jpg",
@@ -104,7 +126,7 @@ public class InitData implements CommandLineRunner {
                     Microchip.NO,
                     clientRepository.getById(Long.parseLong("1")),
                     behaviorRepository.getById(Long.parseLong("1"))));
-
+            //New Dog "Berta"
             dogRepository.save(new Dog("Berta",
                     LocalDate.of(2019, 3, 26),
                     "Berta.jpg",
@@ -115,7 +137,7 @@ public class InitData implements CommandLineRunner {
                     Microchip.YES,
                     clientRepository.getById(Long.parseLong("1")),
                     behaviorRepository.getById(Long.parseLong("5"))));
-
+            //New Dog "Boby"
             dogRepository.save(new Dog("Boby",
                     LocalDate.of(2017, 6, 10),
                     "bok.jpg",
@@ -126,7 +148,7 @@ public class InitData implements CommandLineRunner {
                     Microchip.NO,
                     clientRepository.getById(Long.parseLong("1")),
                     behaviorRepository.getById(Long.parseLong("2"))));
-
+            //New Dog Cezar"
             dogRepository.save(new Dog("Cezar",
                     LocalDate.of(2020, 4, 16),
                     "cezar.jpg",
@@ -137,7 +159,7 @@ public class InitData implements CommandLineRunner {
                     Microchip.YES,
                     clientRepository.getById(Long.parseLong("3")),
                     behaviorRepository.getById(Long.parseLong("4"))));
-
+            //New Dog "Zara"
             dogRepository.save(new Dog("Zara",
                     LocalDate.of(2020, 4, 16),
                     "Zara.jpg",
@@ -148,7 +170,7 @@ public class InitData implements CommandLineRunner {
                     Microchip.NO,
                     clientRepository.getById(Long.parseLong("2")),
                     behaviorRepository.getById(Long.parseLong("7"))));
-
+             //New Dog "Benji"
             dogRepository.save(new Dog("Benji",
                     LocalDate.of(2020, 5, 28),
                     "benji.jpg",
@@ -159,6 +181,17 @@ public class InitData implements CommandLineRunner {
                     Microchip.YES,
                     clientRepository.getById(Long.parseLong("2")),
                     behaviorRepository.getById(Long.parseLong("1"))));
+            //New Dog "Lora"
+            dogRepository.save(new Dog("Lora",
+                    LocalDate.of(2016, 2, 7),
+                    "Lora.jpg",
+                    25,
+                    breedRepository.getById(Long.parseLong("6")),
+                    Sex.F,
+                    Passport.YES,
+                    Microchip.NO,
+                    clientRepository.getById(Long.parseLong("4")),
+                    behaviorRepository.getById(Long.parseLong("5"))));
 
         }
     }
