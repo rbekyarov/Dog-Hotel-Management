27c6ca298de3c3e33f4b53b7f0f4abbedb3bf082
Radoslav Bekyarov <rbekyarov82@gmail.com>
2023-02-28 09:47:57 +0200

    up

--- src/main/java/rbekyarov/project/service/impl/BehaviorServiceImpl.java
+++ src/main/java/rbekyarov/project/service/impl/BehaviorServiceImpl.java
@@ -52,6 +52,7 @@ public class BehaviorServiceImpl implements BehaviorService {
         behavior.setDateCreate(LocalDate.now());
 
         behaviorRepository.save(behavior);
+        List<Behavior> all = behaviorRepository.findAll();
     }
 
     @Override
--- src/test/java/rbekyarov/project/BehaviorServiceTest.java
+++ src/test/java/rbekyarov/project/BehaviorServiceTest.java
@@ -3,11 +3,12 @@ package rbekyarov.project;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.context.junit4.SpringRunner;
 import rbekyarov.project.base.BaseTest;
 import rbekyarov.project.models.dto.BehaviorDTO;
 import rbekyarov.project.models.entity.Behavior;
@@ -15,28 +16,20 @@ import rbekyarov.project.models.entity.User;
 import rbekyarov.project.repository.BehaviorRepository;
 import rbekyarov.project.service.BehaviorService;
 
-import java.util.ArrayList;
 import java.util.List;
-import static org.mockito.Mockito.when;
 
+@RunWith(SpringRunner.class)
 public class BehaviorServiceTest extends BaseTest {
-    List<Behavior> behaviorList;
+
     @Autowired
      BehaviorService behaviorService;
-    @Autowired
-    ModelMapper modelMapper;
 
     @InjectMocks
     MockHttpSession session;
     @MockBean
     BehaviorRepository behaviorRepository;
 
-    @Before
-    public void setupTest() {
-        behaviorList = new ArrayList<>();
-        when(behaviorRepository.findAll())
-                .thenReturn(behaviorList);
-    }
+
     @Before
     public void   initUserSession() {
         User user = new User();
@@ -45,16 +38,17 @@ public class BehaviorServiceTest extends BaseTest {
 
     }
     @Test
-    public void findAllBehavior_whenNoBehavior_returnEmpty() {
-        behaviorList.clear();
+    public void findAllBehaviorReturnEmpty() {
+
         List<Behavior> allBehavior = behaviorService.findAllBehavior();
         Assert.assertTrue(allBehavior.isEmpty());
     }
     @Test
-    public void findAllBehavior_AddBehavior() {
-       behaviorList.clear();
+    public void addBehavior() {
+
         BehaviorDTO behaviorDTO = new BehaviorDTO();
         behaviorDTO.setName("test");
+        behaviorDTO.setId(1l);
 
         this.behaviorService.addBehavior(behaviorDTO,this.session);
         List<Behavior> allBehavior = behaviorRepository.findAll();
