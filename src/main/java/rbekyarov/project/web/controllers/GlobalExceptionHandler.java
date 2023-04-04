4bf1d1a8b4ea4805afbb7b733b9040a4dda9e269
Radoslav Bekyarov <rbekyarov82@gmail.com>
2023-04-04 14:15:27 +0300

    up

--- src/main/java/rbekyarov/project/web/controllers/GlobalExceptionHandler.java
+++ src/main/java/rbekyarov/project/web/controllers/GlobalExceptionHandler.java
@@ -1,24 +1,24 @@
-package rbekyarov.project.web.controllers;
-
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
-
-@ControllerAdvice
-public class GlobalExceptionHandler  {
-
-    @ExceptionHandler({Throwable.class})
-    public ModelAndView handleSqlException(Throwable e){
-        ModelAndView modelAndView = new ModelAndView("error");
-
-        Throwable throwable = e;
-
-        while (throwable.getCause() != null){
-            throwable = throwable.getCause();
-        }
-
-        modelAndView.addObject("message",throwable.getMessage());
-
-        return modelAndView;
-    }
-}
+//package rbekyarov.project.web.controllers;
+//
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.servlet.ModelAndView;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler  {
+//
+//    @ExceptionHandler({Throwable.class})
+//    public ModelAndView handleSqlException(Throwable e){
+//        ModelAndView modelAndView = new ModelAndView("errora.htmle");
+//
+//        Throwable throwable = e;
+//
+//        while (throwable.getCause() != null){
+//            throwable = throwable.getCause();
+//        }
+//
+//        modelAndView.addObject("message",throwable.getMessage());
+//
+//        return modelAndView;
+//    }
+//}
